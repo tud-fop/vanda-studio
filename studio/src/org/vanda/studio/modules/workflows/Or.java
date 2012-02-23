ee40a6285b6c738226def9ae806cd3f907ee3c73
Anja Fischer <afischer@tcs.inf.tu-dresden.de>
2012-02-23 16:47:36 +0100

    OR unfold works on minimal testing example, needs to be generalized

--- studio/src/org/vanda/studio/modules/workflows/Or.java
+++ studio/src/org/vanda/studio/modules/workflows/Or.java
@@ -58,18 +58,28 @@ public class Or implements IElement{
 	
 	@Override
 	public Collection<IHyperworkflow> unfold() {
+		//TODO generalize, works only for minimal example so far: t1->OR, t2->OR, OR->t3
+		
+		
 		List<IHyperworkflow> hwfList = new ArrayList<IHyperworkflow>();
 
+		//get incoming and outgoing connections
+		List<Connection> incoming = new ArrayList<Connection>();
+		List<Connection> outgoing = new ArrayList<Connection>();
+		for (Connection c : parent.getConnections()) {
+			if (c.getTarget().equals(this)) incoming.add(c);
+			if (c.getSource().equals(this)) outgoing.add(c);
+		}
+		
 		for (int i = 0; i < inputPorts.size(); i++) {
 			NestedHyperworkflow parentCopy = new NestedHyperworkflow(parent);	//copy parent NestedHyperworkflow of current or node
 			parentCopy.removeChild(this);	//remove or node
 			
-			// add connection between or-input and or-output
-			//TODO get incoming connection
-			//TODO get outgoing connections
+			for (int j = 0; j < outgoing.size(); j++) {
+				parentCopy.addConnection(new Connection(incoming.get(i).getSource(), incoming.get(i).getSrcPort(), outgoing.get(j).getTarget(), outgoing.get(j).getTargPort()));
+			}
 			
 			hwfList.add(parentCopy);		//add unfolded copy to result list
-			
 		}
 		
 		return hwfList;
