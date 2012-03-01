af23fe5e991009e1d9a3b3db2d7194ef3ed33103
Anja Fischer <s0040641@mail.zih.tu-dresden.de>
2012-03-01 15:49:10 +0100

    just javadoc formatting

--- src/org/vanda/studio/modules/workflows/IHyperworkflow.java
+++ src/org/vanda/studio/modules/workflows/IHyperworkflow.java
@@ -11,16 +11,16 @@ import java.util.List;
 public interface IHyperworkflow {
 
 	public boolean equals(Object other);
-	/** @return the id of the current Hyperworkflow */
+	/** @return the id of the current IHyperworkflow */
 	public int getId();
 	/** @return a list of input ports */
 	public List<Port> getInputPorts();	
-	/** @return the name of the current Hyperworkflow */
+	/** @return the name of the current IHyperworkflow */
 	public String getName();
 	/** @return a list of output ports */
 	public List<Port> getOutputPorts();
-	/** @return the NestedHyperworkflow that contains the current Hyperworkflow */
+	/** @return the NestedHyperworkflow that contains the current IHyperworkflow */
 	public NestedHyperworkflow getParent();
-	/** @return a (duplicate-free) collection of NestedHyperworkflows where all OR nodes have been removed */
+	/** @return a (duplicate-free) collection of IHyperworkflows where all OR nodes have been removed */
 	public Collection<IHyperworkflow> unfold();
 }
