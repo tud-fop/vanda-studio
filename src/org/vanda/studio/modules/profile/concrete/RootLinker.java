e4bde4894988dfa0c95913081c065ff90fbfa156
Matthias Büchse <Matthias.Buechse@tu-dresden.de>
2012-05-22 17:51:56 +0200

    forgot to commit concrete compilers and linkers (RootLinker still under construction)

---
+++ src/org/vanda/studio/modules/profile/concrete/RootLinker.java
@@ -0,0 +1,61 @@
+package org.vanda.studio.modules.profile.concrete;
+
+import java.io.File;
+import java.util.List;
+
+import org.vanda.studio.modules.profile.model.Fragment;
+import org.vanda.studio.modules.profile.model.FragmentIO;
+import org.vanda.studio.modules.profile.model.FragmentLinker;
+
+public class RootLinker implements FragmentLinker {
+
+	@Override
+	public String getCategory() {
+		return "Boxes";
+	}
+
+	@Override
+	public String getContact() {
+		return "Matthias.Buechse@tu-dresden.de";
+	}
+
+	@Override
+	public String getDescription() {
+		return "This box should never be visible to the end user.";
+	}
+
+	@Override
+	public String getId() {
+		return "root-box";
+	}
+
+	@Override
+	public String getName() {
+		return "Root Box";
+	}
+
+	@Override
+	public String getVersion() {
+		return "n/a";
+	}
+
+	@Override
+	public List<String> convertInputs(List<String> outer) {
+		return outer;
+	}
+
+	@Override
+	public List<String> convertOutputs(List<String> inner) {
+		return inner;
+	}
+
+	@Override
+	public Fragment link(Fragment inner, FragmentIO io) {
+		Fragment result = inner.compose();
+		String filename = io.makeUnique(inner.name);
+		File f = io.createFile(filename);
+		f.notify();
+		return result;
+	}
+
+}
