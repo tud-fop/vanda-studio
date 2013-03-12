9157ecee438f2a71caace5df48f6627116592439
Tobias Denkinger <s7480817@mail.zih.tu-dresden.de>
2013-03-12 10:43:40 +0100

    added egretnbestforest

--- share/packages/egret/func.bash
+++ share/packages/egret/func.bash
@@ -1,3 +1,16 @@
+# Egret
+# Version: 2013-03-11
+# Contact: Tobias.Denkinger@mailbox.tu-dresden.de
+# Category: parsing
+# IN corpus :: SentenceCorpus
+# IN grammar :: LAPCFG-Grammar
+# OUT trees :: PennTreeCorpus
+#
+# Computes the best tree for every sentence in the corpus.
+egret () {
+	"$EGRET/egret" -lapcfg "-i=$2" "-data=$4" "-n=1" | sed "/^[(]/!d" > "$5"
+}
+
 # Egret n-best
 # Version: 2013-03-08
 # Contact: Tobias.Denkinger@mailbox.tu-dresden.de
@@ -9,22 +22,19 @@
 #
 # Computes n best trees for the sentences in the corpus.
 egretnbest () {
-if [ 2 -gt "$3" ]; then
-	"$EGRET/egret" -lapcfg "-i=$2" "-data=$4" "-n=1" | sed "/^[(]/!d" > "$5"
-else
 	"$EGRET/egret" -lapcfg "-i=$2" "-data=$4" "-n=$3" | sed "/^[(]/!d" | sed "0~${3}G" | sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' > "$5"
-fi
 }
 
-# Egret
-# Version: 2013-03-11
+# Egret n-best forest
+# Version: 2013-03-08
 # Contact: Tobias.Denkinger@mailbox.tu-dresden.de
 # Category: parsing
 # IN corpus :: SentenceCorpus
+# IN n :: Integer
 # IN grammar :: LAPCFG-Grammar
-# OUT trees :: PennTreeCorpus
+# OUT pcfgs :: PCFGs
 #
-# Computes the best tree for every sentence in the corpus.
-egret () {
-	egretnbest "$1" "$2" "1" "$3" "$4"
+# Computes n best trees for the sentences in the corpus.
+egretnbestforest () {
+	"$EGRET/egret" -lapcfg "-i=$2" "-data=$4" "-nbest4threshold=$3" -printForest | sed "/^$/{n;N;N;d}" | tail -n+3 > "$5"
 }
