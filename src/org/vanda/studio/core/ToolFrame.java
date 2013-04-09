2b6eb3c5381e2587941cb8c0ba049b15d82a3833
Tobias Denkinger <s7480817@mail.zih.tu-dresden.de>
2013-04-09 10:54:42 +0200

    ToolFrame is now (de)iconifiable

--- src/org/vanda/studio/core/ToolFrame.java
+++ src/org/vanda/studio/core/ToolFrame.java
@@ -1,7 +1,10 @@
 package org.vanda.studio.core;
 
+import java.awt.Rectangle;
+
 import javax.swing.JComponent;
 import javax.swing.JInternalFrame;
+import javax.swing.event.InternalFrameEvent;
 
 import org.vanda.studio.app.LayoutAssortment;
 import org.vanda.studio.app.LayoutSelector;
@@ -9,7 +12,7 @@ import org.vanda.studio.app.LayoutSelector;
 @SuppressWarnings("serial")
 class ToolFrame extends JInternalFrame implements LayoutSelector {
 	private final LayoutSelector layout;
-	
+
 	public ToolFrame(JComponent c, LayoutSelector layout) {
 		super(c.getName(), true);
 		this.layout = layout;
@@ -22,5 +25,23 @@ class ToolFrame extends JInternalFrame implements LayoutSelector {
 	@Override
 	public <L> L selectLayout(LayoutAssortment<L> la) {
 		return layout.selectLayout(la);
+	}
+
+	@Override
+	protected void fireInternalFrameEvent(int id) {
+		switch (id) {
+		case InternalFrameEvent.INTERNAL_FRAME_ICONIFIED:
+			setNormalBounds((Rectangle) getBounds().clone());
+			setBounds(getDesktopIcon().getBounds());
+			setResizable(false);
+			break;
+		case InternalFrameEvent.INTERNAL_FRAME_DEICONIFIED:
+			setBounds(getNormalBounds());
+			setResizable(true);
+			break;
+		default:
+			super.fireInternalFrameEvent(id);
+		}
+
 	}
 }
