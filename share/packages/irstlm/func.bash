72c2aff1c10b8460699e7d4eba8f00ecccb10daa
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2013-06-27 10:16:54 +0200

    added tool "IRSTLMScore"

--- share/packages/irstlm/func.bash
+++ share/packages/irstlm/func.bash
@@ -1,6 +1,6 @@
 # IRSTLM
 # Version: 2012-12-03
-# Contact: Tobias.Denkinger@mailbox.tu-dresden.de
+# Contact: Tobias.Denkinger@tu-dresden.de
 # Category: language model
 # IN english corpus :: SentenceCorpus
 # IN n-gram length :: Integer
@@ -15,7 +15,7 @@ IRSTLM () {
 
 # ARPA2Binary
 # Version: 2012-12-04
-# Contact: Tobias Denkinger@mailbox.tu-dresden.de
+# Contact: Tobias Denkinger@tu-dresden.de
 # Category: language model
 # IN texual n-grams :: ARPA
 # OUT binary n-grams :: BinARPA
@@ -25,3 +25,17 @@ ARPA2Binary () {
 	"$IRSTLM/bin/compile-lm" "$2" "$3"
 }
 
+# IRSTLMScore
+# Version: 2013-06-27
+# Contact: Tobias.Denkinger@tu-dresden.de
+# Category: language model
+# IN n-grams :: ARPA
+# IN corpus :: SentenceCorpus
+# OUT logarithmic scores :: Scores
+#
+# Scores sentences according to the language model.
+IRSTLMScore () {
+	"$IRSTLM/bin/add-start-end.sh" < "$3" > "$1/corpus.txt"
+	"$IRSTLM/bin/score-lm" -lm "$2" < "$1/corpus.txt" > "$4"
+	echo "$3" > "${4}.meta"
+}
