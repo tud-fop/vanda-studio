794d6ddc404c8dc8b52b111765fda38185a9059a
Kilian Gebhardt <kilian.gebhardt@mailbox.tu-dresden.de>
2013-08-27 18:14:13 +0200

    tweaking assignment switch tool

--- src/org/vanda/studio/modules/workflows/tools/AssignmentSwitchToolFactory.java
+++ src/org/vanda/studio/modules/workflows/tools/AssignmentSwitchToolFactory.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 
@@ -16,11 +18,13 @@ import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.GroupLayout.ParallelGroup;
 import javax.swing.GroupLayout.SequentialGroup;
+import javax.swing.SwingUtilities;
 
 import org.vanda.studio.app.Application;
 import org.vanda.studio.app.WindowSystem;
@@ -78,6 +82,18 @@ public class AssignmentSwitchToolFactory implements ToolFactory {
 			@Override
 			public void cursorChange(Database d) {
 				aName.setText(d.getName());
+				label.setText(d.getCursor() + 1 + " / " + d.getSize());
+				// enable / disable prev/next-Buttons
+				if (d.getCursor() == 0)
+					prevButton.setEnabled(false);
+				else
+					prevButton.setEnabled(true);
+
+				if (d.getCursor() == d.getSize() - 1)
+					nextButton.setEnabled(false);
+				else
+					nextButton.setEnabled(true);
+
 			}
 
 			@Override
@@ -95,7 +111,9 @@ public class AssignmentSwitchToolFactory implements ToolFactory {
 		private static int ICON_SIZE = 18;
 
 		private JTextField aName;
-		JPanel pan;
+		private JPanel pan;
+		private final JButton prevButton, nextButton;
+		private final JLabel label;
 
 		public JComponent getComponent() {
 			return pan;
@@ -123,11 +141,11 @@ public class AssignmentSwitchToolFactory implements ToolFactory {
 			// Buttons & Textfield
 			PreviousAssignmentAction prev = new PreviousAssignmentAction(db);
 			wfe.addAction(prev, KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, KeyEvent.ALT_MASK), 5);
-			JButton prevButton = createNavigationButton(prev, "arrow-left", wfe.getApplication());
+			prevButton = createNavigationButton(prev, "arrow-left", wfe.getApplication());
 
 			NextAssignmentAction next = new NextAssignmentAction(db);
 			wfe.addAction(next, KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, KeyEvent.ALT_MASK), 6);
-			final JButton nextButton = createNavigationButton(next, "arrow-right", wfe.getApplication());
+			nextButton = createNavigationButton(next, "arrow-right", wfe.getApplication());
 
 			aName = new JTextField() {
 				private static final long serialVersionUID = 3301633459959703449L;
@@ -145,6 +163,23 @@ public class AssignmentSwitchToolFactory implements ToolFactory {
 					db.setName(aName.getText());
 				}
 			});
+			// unfocus text-field when clicking in the surrounding panel
+			pan.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					SwingUtilities.invokeLater(new Runnable() {
+						
+						@Override
+						public void run() {
+							pan.requestFocusInWindow();						
+						}
+					});
+					
+				}
+			});
+
+			label = new JLabel();
+
 			final Listener dl = new Listener();
 
 			// init Label
@@ -160,9 +195,9 @@ public class AssignmentSwitchToolFactory implements ToolFactory {
 			});
 
 			SequentialGroup horizontal = layout.createSequentialGroup().addComponent(prevButton).addComponent(aName)
-					.addComponent(nextButton);
-			ParallelGroup vertical = layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-					.addComponent(prevButton).addComponent(aName).addComponent(nextButton);
+					.addComponent(label).addComponent(nextButton);
+			ParallelGroup vertical = layout.createParallelGroup(GroupLayout.Alignment.CENTER).addComponent(prevButton)
+					.addComponent(aName).addComponent(label).addComponent(nextButton);
 
 			layout.setHorizontalGroup(horizontal);
 			layout.setVerticalGroup(vertical);
