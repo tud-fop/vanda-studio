91500b5606d843a2a7bc55d34f9bd712f93e504e
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2013-10-15 16:22:46 +0200

    util package now assumes the vandarc to source util.bash

--- share/packages/util/install.bash
+++ share/packages/util/install.bash
@@ -5,9 +5,9 @@ binpath="$id"
 
 install_me () {
 	mkdir -p "$VANDASTUDIO" "$BINDIR" "$FUNCDIR" "$IFACEDIR" "$OUTDIR" "$INDIR" "$PKGDB"
-	if [ ! -f $RCPATH ]; then
-		echo -e "source $FUNCDIR/util.bash\n" >> "$RCPATH"
-	fi
+#	if [ ! -f $RCPATH ]; then
+#		echo -e "source $FUNCDIR/util.bash\n" >> "$RCPATH"
+#	fi
 	cp interfaces/* "$IFACEDIR/."
 	cp -r examples/* "$INDIR/."
 }
