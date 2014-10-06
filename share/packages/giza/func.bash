d921dfb5903437edbe3686de7e2c40f143bb0cfe
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2014-10-06 16:40:35 +0200

    changed bash scripts for giza and mgiza to avoid building the whole of mosesdecoder

--- share/packages/giza/func.bash
+++ share/packages/giza/func.bash
@@ -8,18 +8,34 @@
 #
 # computes alignments and translation tables
 GIZA () {
-	rm -rf "$1/*"
-	i1new="$1/corpus.en"
-	i2new="$1/corpus.fr"
-	if [[ ! -f "$i1new" ]]; then
-		ln -svf "$2" "$i1new"
-	fi
-	if [[ ! -f "$i2new" ]]; then
-		ln -svf "$3" "$i2new"
-	fi
-	"$GIZA/mosesdecoder/scripts/training/train-model.perl" -root-dir "$1" --corpus "$1/corpus" --e fr --f en --last-step 3 --external-bin-dir="$GIZA/giza"
-	mv "$1/model/aligned.grow-diag-final" "$4"
-	unlink "$i1new"
-	unlink "$i2new"
+	## this script reimplements the corresponding functionality in mosesdecoder
+	
+	## (1) prepare data
+	mkdir -p "$1"
+
+	"$GIZA/mkcls" -c50 -n2 "-p$2" "-V$1/en.vcb.classes" opt
+	"$GIZA/mkcls" -c50 -n2 "-p$3" "-V$1/fr.vcb.classes" opt
+
+	"$GIZA/plain2snt.out" "$2" "$3"
+	mv "$2.vcb" "$1/en.vcb"
+	mv "$3.vcb" "$1/fr.vcb"
+	mv "$2_$(basename $3).snt" "$1/en-fr-int-train.snt"
+	mv "$3_$(basename $2).snt" "$1/fr-en-int-train.snt"
+
+	
+	## (2) run GIZA++
+	mkdir -p "$1/giza.en-fr"
+	"$GIZA/snt2cooc.out" "$1/fr.vcb" "$1/en.vcb" "$1/en-fr-int-train.snt" > "$1/giza.en-fr/en-fr.cooc"
+
+	"$GIZA/GIZA++" -CoocurrenceFile "$1/giza.en-fr/en-fr.cooc" -c "$1/en-fr-int-train.snt" -m1 5 -m2 0 -m3 3 -m4 3 -model1dumpfrequency 1 -model4smoothfactor 0.4 -nodumps 1 -nsmooth 4 -o "$1/giza.en-fr/en-fr" -onlyaldumps 1 -p0 0.999 -s "$1/en.vcb" -t "$1/fr.vcb"
+
+	mkdir -p "$1/giza.fr-en"
+	"$GIZA/snt2cooc.out" "$1/en.vcb" "$1/fr.vcb" "$1/fr-en-int-train.snt" > "$1/giza.fr-en/fr-en.cooc"
+
+	"$GIZA/GIZA++" -CoocurrenceFile "$1/giza.fr-en/fr-en.cooc" -c "$1/fr-en-int-train.snt" -m1 5 -m2 0 -m3 3 -m4 3 -model1dumpfrequency 1 -model4smoothfactor 0.4 -nodumps 1 -nsmooth 4 -o "$1/giza.fr-en/fr-en" -onlyaldumps 1 -p0 0.999 -s "$1/fr.vcb" -t "$1/en.vcb"
+
+	## (3) align words
+	"$GIZA/giza2bal.pl" -d "$1/giza.en-fr/en-fr.A3.final" -i "$1/giza.fr-en/fr-en.A3.final" | "$GIZA/symal" -alignment="grow" -diagonal="yes" -final="yes" -both="no" > "$4"
+
 	echo -e "$3\n$2\n" > "${4}.meta"
 }
--- share/packages/mgiza/func.bash
+++ share/packages/mgiza/func.bash
@@ -8,18 +8,38 @@
 #
 # computes alignments and translation tables
 GIZA () {
-	rm -rf "$1/*"
-	i1new="$1/corpus.en"
-	i2new="$1/corpus.fr"
-	if [[ ! -f "$i1new" ]]; then
-		iconv -f utf-8 -t ASCII//TRANSLIT "$2" > "$i1new"
-	fi
-	if [[ ! -f "$i2new" ]]; then
-		iconv -f utf-8 -t ASCII//TRANSLIT "$3" > "$i2new"
-	fi
-	"$MGIZA/mosesdecoder/scripts/training/train-model.perl" -mgiza -root-dir "$1" --corpus "$1/corpus" --e fr --f en --last-step 3 --external-bin-dir="$MGIZA/giza"
-	mv "$1/model/aligned.grow-diag-final" "$4"
-#	unlink "$i1new"
-#	unlink "$i2new"
+	## this script reimplements the corresponding functionality in mosesdecoder
+	
+	## (1) prepare data
+	mkdir -p "$1"
+
+	"$MGIZA/mkcls" -c50 -n2 "-p$2" "-V$1/en.vcb.classes" opt
+	"$MGIZA/mkcls" -c50 -n2 "-p$3" "-V$1/fr.vcb.classes" opt
+
+	"$MGIZA/plain2snt.out" "$2" "$3"
+	mv "$2.vcb" "$1/en.vcb"
+	mv "$3.vcb" "$1/fr.vcb"
+	mv "$2_$(basename $3).snt" "$1/en-fr-int-train.snt"
+	mv "$3_$(basename $2).snt" "$1/fr-en-int-train.snt"
+
+	
+	## (2) run GIZA++
+	mkdir -p "$1/giza.en-fr"
+	"$MGIZA/snt2cooc.out" "$1/fr.vcb" "$1/en.vcb" "$1/en-fr-int-train.snt" > "$1/giza.en-fr/en-fr.cooc"
+
+	"$MGIZA/mgiza" -CoocurrenceFile "$1/giza.en-fr/en-fr.cooc" -c "$1/en-fr-int-train.snt" -m1 5 -m2 0 -m3 3 -m4 3 -model1dumpfrequency 1 -model4smoothfactor 0.4 -ncpus "${NPROC}" -nodumps 1 -nsmooth 4 -o "$1/giza.en-fr/en-fr" -onlyaldumps 1 -p0 0.999 -s "$1/en.vcb" -t "$1/fr.vcb"
+
+	"$MGIZA/merge_alignment.py" $1/giza.en-fr/en-fr.A3.final.part* > "$1/giza.en-fr/en-fr.A3.final"
+
+	mkdir -p "$1/giza.fr-en"
+	"$MGIZA/snt2cooc.out" "$1/en.vcb" "$1/fr.vcb" "$1/fr-en-int-train.snt" > "$1/giza.fr-en/fr-en.cooc"
+
+	"$MGIZA/mgiza" -CoocurrenceFile "$1/giza.fr-en/fr-en.cooc" -c "$1/fr-en-int-train.snt" -m1 5 -m2 0 -m3 3 -m4 3 -model1dumpfrequency 1 -model4smoothfactor 0.4 -ncpus "${NPROC}" -nodumps 1 -nsmooth 4 -o "$1/giza.fr-en/fr-en" -onlyaldumps 1 -p0 0.999 -s "$1/fr.vcb" -t "$1/en.vcb"
+
+	"$MGIZA/merge_alignment.py" $1/giza.fr-en/fr-en.A3.final.part* > "$1/giza.fr-en/fr-en.A3.final"
+
+	## (3) align words
+	"$MGIZA/giza2bal.pl" -d "$1/giza.en-fr/en-fr.A3.final" -i "$1/giza.fr-en/fr-en.A3.final" | "$MGIZA/symal" -alignment="grow" -diagonal="yes" -final="yes" -both="no" > "$4"
+
 	echo -e "$3\n$2\n" > "${4}.meta"
 }
