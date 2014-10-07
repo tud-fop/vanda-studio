c7d01478feed479c7bd7ebc462692dcf493aea70
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2014-10-07 09:31:02 +0200

    added XRSNGramsTranslate

--- share/packages/vanda/func.bash
+++ share/packages/vanda/func.bash
@@ -39,6 +39,26 @@ XRSNGrams () {
 	"$VANDA/programs/XRSToHypergraph" b2t -e "$1/map.e" -f "$1/map.f" -z "$1/zhg.new" > "$4"
 }
 
+# XRSNGramsTranslate
+# Version: 2014-10-07
+# Contact: Tobias.Denkinger@tu-dresden.de
+# Category: translation
+# IN corpus :: SentenceCorpus
+# IN rules :: GHKMHypergraph
+# IN ngrams :: ARPA
+# IN beam :: Integer
+# OUT translation :: SentenceCorpus
+#
+# Translates a SentenceCorpus using a GHKM hypergraph and a language model.
+XRSNGramsTranslate () {
+	"$VANDA/programs/XRSToHypergraph" t2b -e "$1/map.e" -f "$1/map.f" -z "$1/zhg" < "$3"
+	if [ "$5" -eq "0" ]; then
+		"$VANDA/programs/XRSNGrams" -t "NoBackoff" -e "$1/map.e" -f "$1/map.f" -z "$1/zhg" -l "$4" < "$2" > "$6"
+	else
+		"$VANDA/programs/XRSNGrams" -t "Pruning" -b "$5" -e "$1/map.e" -f "$1/map.f" -z "$1/zhg" -l "$4" < "$2" > "$6"
+	fi
+}
+
 # NGrams
 # Version: 2013-01-31
 # Contact: Tobias.Denkinger@tu-dresden.de
