967db49087fbec2e767f67de04d8c06223b0c3ce
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2014-10-27 16:11:08 +0100

    berkeley now also generates preview files

--- share/packages/implementations/berkeley/install.bash
+++ share/packages/implementations/berkeley/install.bash
@@ -4,14 +4,12 @@ version="2014-10-09"
 binpath="$id"
 
 download () {
-	wget -N "http://berkeleyparser.googlecode.com/files/berkeleyParser.jar"
+	wget -nc "http://berkeleyparser.googlecode.com/files/berkeleyParser.jar"
 	mkdir -p "berkeley_grammars"
-	wget -P "berkeley_grammars" -nc "http://berkeleyparser.googlecode.com/files/eng_sm6.gr"
-	wget -P "berkeley_grammars" -nc "http://berkeleyparser.googlecode.com/files/bul_sm5.gr"
-	wget -P "berkeley_grammars" -nc "http://berkeleyparser.googlecode.com/files/arb_sm5.gr"
-	wget -P "berkeley_grammars" -nc "http://berkeleyparser.googlecode.com/files/chn_sm5.gr"
-	wget -P "berkeley_grammars" -nc "http://berkeleyparser.googlecode.com/files/fra_sm5.gr"
-	wget -P "berkeley_grammars" -nc "http://berkeleyparser.googlecode.com/files/ger_sm5.gr"
+	for name in eng_sm6 bul_sm5 arb_sm5 chn_sm5 fra_sm5 ger_sm5; do
+		wget -P "berkeley_grammars" -nc "http://berkeleyparser.googlecode.com/files/${name}.gr"
+		java -cp "berkeleyParser.jar" "edu/berkeley/nlp/PCFGLA/WriteGrammarToTextFile" "berkeley_grammars/${name}.gr" "berkeley_grammars/${name}.gr.prev"
+	done
 }
 
 install_me () {
@@ -20,3 +18,4 @@ install_me () {
 	cp "src/Main.class" "$1/."
 	cp --recursive "berkeley_grammars" "$2/."
 }
+
