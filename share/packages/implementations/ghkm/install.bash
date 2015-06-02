64f27d3299c7c077ef51fdf4c15f9f16c836e292
Toni Dietze <toni.dietze@tu-dresden.de>
2015-06-02 15:41:05 +0200

    share/packages/implementations/ghkm/install.bash: fixed download links

--- share/packages/implementations/ghkm/install.bash
+++ share/packages/implementations/ghkm/install.bash
@@ -1,13 +1,17 @@
 id="ghkm"
 varname="GHKM"
-version="2013-03-05"
+version="2015-06-02"
 binpath="$id"
 
 download () {
-	wget -O - "http://www-nlp.stanford.edu/~mgalley/software/stanford-ghkm-latest.tar.gz" | tar xz --wildcards 'stanford-ghkm-*/ghkm.jar' 'stanford-ghkm-*/lib/fastutil.jar'
+	rm -fr 'downloads'
+	mkdir 'downloads'
+	cd 'downloads'
+	wget 'https://github.com/joshua-decoder/joshua/raw/master/lib/fastutil.jar'
+	wget 'https://github.com/joshua-decoder/joshua/raw/master/lib/ghkm-modified.jar'
 }
 
 install_me () {
-	cp stanford-ghkm-*/ghkm.jar "$1/."
-	cp stanford-ghkm-*/lib/fastutil.jar "$1/."
+	cp 'downloads/fastutil.jar'      "$1/fastutil.jar"
+	cp 'downloads/ghkm-modified.jar' "$1/ghkm.jar"
 }
