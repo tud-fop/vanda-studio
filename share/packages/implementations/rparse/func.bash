tud-fop/vanda-studio

commit 07ee11482347d6af5304620145e167e3feba9753

RparseLCFRS show output in rcg format
Build rparse new for correct output

share/packages/implementations/rparse/func.bash
@@ -28,5 +28,7 @@ rparseTrain () {
        -doTrain \
        -train "$2" \
        -headFinder negra \
-       -saveModel "$3"
+       -saveModel "$3" \
+       -trainSave "$3" \
+       -trainSaveFormat rcg
 }

src/org/vanda/studio/modules/previews/PreviewsModule.java
@@ -32,6 +32,8 @@ public class PreviewsModule implements Module {
 				new AlignmentsPreviewFactory(app));
 		app.registerPreviewFactory(new CompositeType("PLCFRS"),
 				new PLCFRSPreviewFactory(app, ".readable"));
+		app.registerPreviewFactory(new CompositeType("RparseLCFRS"),
+				new RparseLCFRSPreviewFactory(app, "/grammarrcg"));
 		app.registerPreviewFactory(new CompositeType("NegraCorpus"),
 				new NegraTreePreviewFactory(app, ".penn"));
 		app.registerPreviewFactory(new CompositeType("log"),

src/org/vanda/studio/modules/previews/RparseLCFRSPreviewFactory.java
@@ -1,0 +1,165 @@
+package org.vanda.studio.modules.previews;
+
+import java.awt.Component;
+import java.awt.Desktop;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+import org.vanda.studio.app.Application;
+import org.vanda.studio.app.PreviewFactory;
+
+public class RparseLCFRSPreviewFactory implements PreviewFactory {
+
+	private class InsetListCellRenderer extends DefaultListCellRenderer {
+		@Override
+		public Component getListCellRendererComponent(JList list, Object value,
+				int index, boolean isSelected, boolean cellHasFocus) {
+			JLabel cell = (JLabel) super.getListCellRendererComponent(list,
+					value, index, isSelected, cellHasFocus);
+			cell.setBorder(BorderFactory.createEmptyBorder(2, 10, 2, 10));
+			return cell;
+		}
+	}
+
+	public class RparseLCFRSPreview extends JList {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private DefaultListModel model;
+		private Scanner fs;
+		private JButton bMore;
+		private static final int SIZE = 20;
+		private static final String MORE = "[show more rules]";
+
+		public RparseLCFRSPreview(String value, String postfix) {
+			super();
+			setCellRenderer(new InsetListCellRenderer());
+			model = new DefaultListModel();
+			setLayoutOrientation(JList.HORIZONTAL_WRAP);
+			setVisibleRowCount(-1);
+			addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent evt) {
+					if (evt.getClickCount() == 2
+							&& model.get(locationToIndex(evt.getPoint())) == MORE)
+						more();
+				}
+			});
+			try {
+				fs = new Scanner(new FileInputStream(app.findFile(value
+						+ postfix)));
+				fs.nextLine();
+				fs.nextLine();
+				fs.nextLine();
+				fs.nextLine();
+				more();
+			} catch (FileNotFoundException e) {
+				add(new JLabel(value + postfix + " does not exist."));
+			}
+		}
+
+		public void more() {
+			if (fs == null)
+				return;
+			model.removeElement(MORE);
+			String[] l;
+			int i = 0;
+			while (i < SIZE & fs.hasNextLine()) {
+				String txt = "<html>";
+				l = fs.nextLine().split(" ");
+				for (String s : l) {
+					if (s.equals("->"))
+						txt += " &#10230; ";
+					else if (l[l.length - 1] == s) {
+						if (s.contains("e"))
+							txt += "  [" + cut(s.split("e")[0], 5)
+									+ " &middot; 10<sup>" + s.split("e")[1]
+									+ "</sup>]";
+						else
+							txt += "  ["
+									+ cut(s, 8)
+									+ "]";
+
+					} else if (s.contains("_")) {
+						txt += s.replace("_",
+								"<sub>")
+								+ "</sub> ";
+					}
+					else
+						txt += s + " ";
+				}
+				txt += "</html>";
+				model.addElement(txt);
+				i++;
+			}
+			if (fs.hasNext())
+				model.addElement(MORE);
+			setModel(model);
+		}
+
+		@Override
+		public void finalize() {
+			fs.close();
+		}
+		
+		private String cut(String in, int n) {
+			if (in.length() <= n)
+				return in;
+			else
+				return in.substring(0, n);
+		}
+	}
+
+	private String postfix;
+	private Application app;
+
+	public RparseLCFRSPreviewFactory(Application app, String postfix) {
+		super();
+		this.app = app;
+		this.postfix = postfix;
+	}
+
+	@Override
+	public JComponent createPreview(String value) {
+		return new JScrollPane(new RparseLCFRSPreview(value, postfix));
+	}
+
+	@Override
+	public void openEditor(final String value) {
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Desktop.getDesktop().open(new File(value + postfix));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		t.start();
+	}
+
+	@Override
+	public JComponent createSmallPreview(String value) {
+		return new JScrollPane(new RparseLCFRSPreview(value, postfix));
+	}
+
+}

commit 4e9d3545304d02c9656c80cdcda1dc7b9875883c

fix broken rparse training introduced in 07ee114 can't store binary and readable grammar files in same dir

share/packages/implementations/rparse/func.bash
@@ -29,6 +29,6 @@ rparseTrain () {
        -train "$2" \
        -headFinder negra \
        -saveModel "$3" \
-       -trainSave "$3" \
+       -trainSave "$3.readable" \
        -trainSaveFormat rcg
 }

src/org/vanda/studio/modules/previews/PreviewsModule.java
@@ -33,7 +33,7 @@ public class PreviewsModule implements Module {
 		app.registerPreviewFactory(new CompositeType("PLCFRS"),
 				new PLCFRSPreviewFactory(app, ".readable"));
 		app.registerPreviewFactory(new CompositeType("RparseLCFRS"),
-				new RparseLCFRSPreviewFactory(app, "/grammarrcg"));
+				new RparseLCFRSPreviewFactory(app, ".readable/grammarrcg"));
 		app.registerPreviewFactory(new CompositeType("NegraCorpus"),
 				new NegraTreePreviewFactory(app, ".penn"));
 		app.registerPreviewFactory(new CompositeType("log"),